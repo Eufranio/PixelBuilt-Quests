44ed93a05701b4bf2ab27814fefdc50f230a16b3
Eufranio <eufraniow@gmail.com>
2025-03-18 20:30:27 -0300

    Add support for beconomy (fabric) and fix initialization error due to shadowing

--- fabric/src/main/java/dev/eufranio/pixelbuiltquests/fabric/PixelBuiltQuestsFabric.java
+++ fabric/src/main/java/dev/eufranio/pixelbuiltquests/fabric/PixelBuiltQuestsFabric.java
@@ -2,15 +2,18 @@ package dev.eufranio.pixelbuiltquests.fabric;
 
 import dev.eufranio.pixelbuiltquests.PixelBuiltQuests;
 import dev.eufranio.pixelbuiltquests.economy.QuestsEconomyProvider;
-import dev.eufranio.pixelbuiltquests.fabric.economy.BlanketEconomyImpl;
+import dev.eufranio.pixelbuiltquests.fabric.economy.BEconomyImpl;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.LoggerFactory;
 
 public final class PixelBuiltQuestsFabric implements ModInitializer {
 
     public PixelBuiltQuestsFabric() {
-        if (FabricLoader.getInstance().isModLoaded("blanketeconomy")) {
-            QuestsEconomyProvider.register(new BlanketEconomyImpl());
+        if (FabricLoader.getInstance().isModLoaded("beconomy")) {
+            LoggerFactory.getLogger(PixelBuiltQuests.MOD_ID)
+                    .info("Registering PBQ Economy Integration with BEconomy");
+            QuestsEconomyProvider.register(new BEconomyImpl());
         }
         new PixelBuiltQuests();
     }
--- fabric/src/main/java/dev/eufranio/pixelbuiltquests/fabric/economy/BEconomyImpl.java
+++ fabric/src/main/java/dev/eufranio/pixelbuiltquests/fabric/economy/BEconomyImpl.java
@@ -1,33 +1,33 @@
 package dev.eufranio.pixelbuiltquests.fabric.economy;
 
 import dev.eufranio.pixelbuiltquests.economy.QuestsEconomy;
-import org.blanketeconomy.api.BlanketEconomy;
+import org.beconomy.api.BEconomy;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public class BlanketEconomyImpl implements QuestsEconomy {
+public class BEconomyImpl implements QuestsEconomy {
 
     @Override
     public boolean has(UUID player, int value) {
-        return BlanketEconomy.INSTANCE.getAPI()
+        return BEconomy.INSTANCE.getAPI()
                 .hasEnoughFunds(player, new BigDecimal(value), getCurrencyName());
     }
 
     @Override
     public void withdraw(UUID player, int value) {
-        BlanketEconomy.INSTANCE.getAPI()
+        BEconomy.INSTANCE.getAPI()
                 .subtractBalance(player, new BigDecimal(value), getCurrencyName());
     }
 
     @Override
     public void deposit(UUID player, int value) {
-        BlanketEconomy.INSTANCE.getAPI()
+        BEconomy.INSTANCE.getAPI()
                 .addBalance(player, new BigDecimal(value), getCurrencyName());
     }
 
     String getCurrencyName() {
-        return BlanketEconomy.INSTANCE.getAPI()
+        return BEconomy.INSTANCE.getAPI()
                 .getPrimaryCurrency()
                 .getCurrencyType();
     }
